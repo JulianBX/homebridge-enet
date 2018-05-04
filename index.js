8a2c0f3d67bc4ae0de7e23f5fb7ea972c2554583
JulianBX <34688834+JulianBX@users.noreply.github.com>
2018-05-04 11:04:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,19 +10,17 @@ module.exports = function (homebridge) {
     Accessory = homebridge.platformAccessory;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform("homebridge-eNet-dev", "eNetPlatform-dev", eNetPlatform); //, true);
+    homebridge.registerPlatform("homebridge-eNet", "eNetPlatform", eNetPlatform); //, true);
 }
 
 
 function eNetPlatform(log, config, api) {
-    var eNetPlatform = this;
     this.log = log;
     this.config = config;
     this.accessories = [];
     this.delAccessories = [];
     this.gateways = [];
     this.loadState = 2; // didFinishLaunching & discover
-
 
     var discover = new eNet.discover();
 
@@ -38,10 +36,9 @@ function eNetPlatform(log, config, api) {
     else --this.loadState;
 
     discover.discover(function(err) {
-        if (err) console.log('Discovery error: ' + err);
+        if (err) log.console.warn('Discovery error: ' + err);
         if (--this.loadState === 0) this.setupDevices();
     }.bind(this));
-
 };
 
 eNetPlatform.prototype.newGateway = function (gw) {
@@ -82,7 +79,6 @@ eNetPlatform.prototype.setupDevices = function() {
                         {
                             if (res && Array.isArray(res.DEVICES)) {
                                 g.devices = res.DEVICES;
-       				console.log("res.DEVICES are" + JSON.stringify(res));
                                 this.gateways.push(g);
                             }
                             else err = JSON.stringify(res);
@@ -124,7 +120,7 @@ eNetPlatform.prototype.setupDevices = function() {
             else this.log.warn("Gateway has no accessories: " + JSON.stringify(gw));
         }
     }
-    else this.log.warn("No gateways defined: " + JSON.stringify(this.config));
+    else this.log.warn("No gateways defined: " + JSOM.stringify(this.config));
 
     var keep = [];
     for (var i = 0; i < this.accessories.length; ++i) {
@@ -136,19 +132,17 @@ eNetPlatform.prototype.setupDevices = function() {
             if (service = acc.getService(Service.Lightbulb)) {
                 if (acc.context.dimmable) {
                     this.log.info("Configuring brightness for " + acc.context.name);
-		    acc.brightness = 100;
+                    acc.brightness = 100;
 
-                    service
-			.getCharacteristic(Characteristic.Brightness)
-                      	.on('get', getBrightness.bind(acc))
-                      	.on('set', setBrightness.bind(acc));
-                }
-		else {
+                    service.getCharacteristic(Characteristic.Brightness)
+                      .on('get', getBrightness.bind(acc))
+                      .on('set', setBrightness.bind(acc));
+                } else {
                     service.removeCharacteristic(Characteristic.Brightness);
                 }
             }
             if (acc.context.type  === "Light") {
-	    }
+            }
         }
         else {
             this.log.info("Deleting old accessory: " + JSON.stringify(acc.context));
@@ -156,14 +150,13 @@ eNetPlatform.prototype.setupDevices = function() {
         }
     }
 
-    if (this.delAccessories.length) this.api.unregisterPlatformAccessories("homebridge-eNet-dev", "eNetPlatform-dev", this.delAccessories);
+    if (this.delAccessories.length) this.api.unregisterPlatformAccessories("homebridge-eNet", "eNetPlatform", this.delAccessories);
     this.delAccessories = [];
     this.accessories = keep;
 
     this.log.info("Platform initialization finishd: " + this.accessories.length + " accessories available.");
-    this.log.info("Signing in now!");
 
-    //HIER SIGN IN!!!!!!!!!!!   #####################################
+
     var accChannels = [];
     for (var k = 0; k < this.accessories.length; k++) {
 	accChannels.push(this.accessories[k].context.channel);
@@ -171,84 +164,24 @@ eNetPlatform.prototype.setupDevices = function() {
 		console.log("Eventlistener for: " + this.accessories[k].context.name);
 	}.bind(this));
     }
-    ///###################################################################
+
     for (var i = 0; i < this.gateways.length; i++) {
         var gw = this.gateways[i];
-	gw.updateAccessories(this.accessories);  //Gateway kennt seine Accesories!
 	gw.signIn(accChannels);
-	gw.on('UpdateAvailableForChannel', function (correctedChannelAsIndex, values) {
-		this.updateChannel(correctedChannelAsIndex, values);
-		//var service = this.accessories[correctedChannelAsIndex].getService(Service.Lightbulb);
-		//service.getCharacteristic(Characteristic.On).updateValue(values.STATE);
-		//service.getCharacteristic(Characteristic.Brightness).updateValue(values.VALUE);
+	gw.on('UpdateAvailable', function (obje) {
+		if(this.accessories[obje.NUMBER-16].context.channel == obje.NUMBER) {
+			var service = this.accessories[obje.NUMBER-16].getService(Service.Lightbulb);
+			service.getCharacteristic(Characteristic.On).updateValue(obje.STATE);
+                        service.getCharacteristic(Characteristic.Brightness).updateValue(obje.VALUE);
+			var newValue= (obje.STATE=== "ON" ? true : false);
+                        this.accessories[obje.NUMBER-16].realOn = newValue;
+			this.accessories[obje.NUMBER-16].brightness = obje.VALUE;
+		}
 	}.bind(this));
-
-
-
-
-/*	gw.on('UpdateAvailable', function (obje){
-//Muss überarbeitet werden -> emitter und so.... gibt mega delay!
-		this.log.info("Received an update!" + JSON.stringify(obje));
-		for (var i = 0; i < obje.length; i++) {
-//			console.log("Doing" + obje[i].NUMBER);
-			var service = this.accessories[obje[i].NUMBER-16].getService(Service.Lightbulb);
-			if(service){
-                                var state = (obje[i].STATE == "ON" ? true : false);
-                                console.log("Channel: " + obje[i].NUMBER + " State: " + state + " Value: " + obje[i].VALUE);
-				this.receivedState = state;
-                                service.getCharacteristic(Characteristic.On).updateValue(state);
-				this.brightness = obje[i].VALUE;
-                                service.getCharacteristic(Characteristic.Brightness).updateValue(obje[i].VALUE);
-                        }
-		}
-	}.bind(this));*/
     }
+
+
 }
-
-eNetPlatform.prototype.updateChannel = function(correctedChannelAsIndex, values) {
-	values.TIME = Math.floor(Date.now()/100);
-	//{"NUMBER":"16","VALUE":"30","STATE":"ON","SETPOINT":"255","TIME":15143003617}
-	console.log("UPDATES: " + JSON.stringify(values));
-
-	var service = this.accessories[correctedChannelAsIndex].getService(Service.Lightbulb);
-    var state = (values.UPDATES.STATE === "ON" ? true : false);
-	console.log("Service: " + service + " State: " + state);	
-//		service.getCharacteristic(Characteristic.On).updateValue(state);
-//              service.getCharacteristic(Characteristic.Brightness).updateValue(this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-//		if(this.updateQueue[correctedChannelAsIndex]) clearTimeout(this.updateQueue[correctedChannelAsIndex]);
-/*		else { 	//Wenn es noch keinen Timer gab...dann senden wir das erste Update direkt!
-			//Dann werden die Updates gequeued.
-			service.getCharacteristic(Characteristic.On).updateValue(state);
-                        service.getCharacteristic(Characteristic.Brightness).updateValue(this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-			console.log("Immediately sending the update: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.NUMBER);
-		}
-        	//Timer clearen wenn es ihn gibt und der gerade empfangene Datensatz nicht dem schon gespeichertem entspricht.
-        	//if(!this.updateQueue[correctedChannelAsIndex]){ //Wenn schon ein Timer gesetzt wurde ersetzen wir ihn nicht!
-        	        this.updateQueue[correctedChannelAsIndex] = setTimeout(function() {
-        	  		service.getCharacteristic(Characteristic.On).updateValue(state);
-			        service.getCharacteristic(Characteristic.Brightness).updateValue(this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-        	                this.updateQueue[correctedChannelAsIndex] = null;
-				console.log("Just sending the update: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.NUMBER + " to state: " + state + " value: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-        	        }.bind(this), 1000);
-        	//}
-	}
-//        console.log("Just sending the update: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.NUMBER + " to state: " + state + " value: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-/*	else if(this.updateQueue[correctedChannelAsIndex]) clearTimeout(this.updateQueue[correctedChannelAsIndex]);
-	//Timer clearen wenn es ihn gibt und der gerade empfangene Datensatz nicht dem schon gespeichertem entspricht.
-	if(!this.updateQueue[correctedChannelAsIndex]){ //Wenn schon ein Timer gesetzt wurde ersetzen wir ihn nicht!
-		this.updateQueue[correctedChannelAsIndex] = setTimeout(function() {
-			var service = this.accessories[correctedChannelAsIndex].getService(Service.Lightbulb);
-			var state = (this.latestUpdates[correctedChannelAsIndex].UPDATES.STATE === "ON" ? true : false);
-	        	service.getCharacteristic(Characteristic.On).updateValue(state);
-        		service.getCharacteristic(Characteristic.Brightness).updateValue(this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-    			console.log("Just sending the update: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.NUMBER + " to state: " + state + " value: " + this.latestUpdates[correctedChannelAsIndex].UPDATES.VALUE);
-			this.updateQueue[correctedChannelAsIndex] = null;
-		}.bind(this), 1000);
-	}
-*/
-}
-
-
 
 eNetPlatform.prototype.findGateway = function(id) {
     for (var i = 0; i < this.gateways.length; ++i) {
@@ -264,9 +197,6 @@ eNetPlatform.prototype.findAccessory = function(gateID, channel) {
     }
 }
 
-// Function invoked when homebridge tries to restore cached accessory.
-// Developer can configure accessory at here (like setup event handler).
-// Update current value.
 eNetPlatform.prototype.configureAccessory = function(accessory) {
     this.log.info("Configure accessory: " + JSON.stringify(accessory.context));
     if (this.setupAccessory(accessory)) {
@@ -293,7 +223,8 @@ eNetPlatform.prototype.createAccessory = function(gate, conf) {
         accessory.addService(Service.Window, conf.name)
     }
     else if (conf.type === "Light") {
-        accessory.addService(Service.Lightbulb, conf.name)
+        accessory.addService(Service.Lightbulb, conf.name);
+	accessory.realOn = false;
     }
     else if (conf.type === "Switch") {
         accessory.addService(Service.Switch, conf.name)
@@ -309,22 +240,18 @@ eNetPlatform.prototype.createAccessory = function(gate, conf) {
     accessory.context.name = conf.name;
     accessory.context.duration = conf.duration;
     accessory.context.dimmable = conf.dimmable;
-    accessory.receivedBrightness = 0;
-    accessory.receivedState = false;
-    accessory.positionState = false;
-
 
     if (this.setupAccessory(accessory)) {
         accessory.reachable = true;
         accessory.gateway = gate;
         this.accessories.push(accessory);
-        this.api.registerPlatformAccessories("homebridge-eNet-dev", "eNetPlatform-dev", [accessory]);
+        this.api.registerPlatformAccessories("homebridge-eNet", "eNetPlatform", [accessory]);
     }
 }
 
 eNetPlatform.prototype.setupAccessory = function(accessory) {
     var service;
-    console.log("setting up the accesory");
+
     accessory.log = this.log;
 
     if (accessory.getService(Service.WindowCovering)) {
@@ -336,9 +263,10 @@ eNetPlatform.prototype.setupAccessory = function(accessory) {
 
     if (service = accessory.getService(Service.Lightbulb)) {
         service
-		.getCharacteristic(Characteristic.On)
-                .on('set', setOn.bind(accessory))
-		.on('get', getOn.bind(accessory));
+          .getCharacteristic(Characteristic.On)
+          .on('set', setOn.bind(accessory))
+	  .on('get', getOn.bind(accessory));
+
     }
     else if (service = accessory.getService(Service.Switch)) {
         service
@@ -375,6 +303,17 @@ eNetPlatform.prototype.setupAccessory = function(accessory) {
 
     accessory.on('identify', function(paired, callback) {
         this.log.info("Identify: " + JSON.stringify(this.context));
+        callback();
+    }.bind(accessory));
+
+    accessory.on(accessory.context.channel, function(callback, obj) {
+        this.log.info("Received update! : " + JSON.stringify(this.context));
+	if(obj.STATE) {
+	     this.setState(obj.STATE === "ON" ? true : false);
+	}
+	else if (obj.VALUE) {
+		this.setValueDim(obj.VALUE);
+	}
         callback();
     }.bind(accessory));
 
@@ -433,15 +372,12 @@ function getPositionState(callback) {
   callback(null, this.positionState);
 }
 
+
 function getOn(callback) {
-  if (!this.gateway) {
-    this.log.warn("eNet device not ready.");
-    callback(new Error("eNet device not ready."));
-    return;
-  }
-  this.log.info("getOn " + this.context.name + ": " + this.receivedState);
-  //this.positionState = this.receivedState;
-  callback(null, this.receivedState);
+//  this.gateway.signIn();
+  console.log(this.accessories);
+  this.log.info("getOn " + this.context.name + ": " + this.realOn);
+  callback(null, this.realOn);
 }
 
 function setOn(position, callback) {
@@ -450,66 +386,65 @@ function setOn(position, callback) {
     callback(new Error("eNet device not ready."));
     return;
   }
-  this.log.info("Setting " + this.context.name + " to " + position);
+
+  this.log.info("Setting " + this.context.name + " to " + (position === true ? "on" : "off"));
 
   callback(null);
-  this.log.info("Ich war bereits: " + this.receivedState);
-  setTimeout(function() {}, 10000);
-  this.log.info("Genug gewartet.");
-//  if( position !== true && position !== false) {}
-  if(this.receivedState === true && position === true) {} //Wenn aktuell schon an wenn schon aus, dann setze nochmal aus.
-  else if( this.receivedState === false && position === false) {} //Wenn es gedimmt werden soll entspricht value einer Zahl und keinem boolean
-  else {
-	if (position === true || position === false){
-		this.receivedState = position;
-		var service = this.getService(Service.Lightbulb) || this.getService(Service.Switch);
-                if (service) {
-			service.getCharacteristic(Characteristic.On).setValue(position);
-		}
+  var lightbulb = this.getService(Service.Lightbulb);
+  if(lightbulb) {
+  	if((position == true || position > 0) &&  this.realOn != true) {
+        	this.realOn = true;
+//		console.log("Es werde Licht.");
+  		this.gateway.setValue(this.context.channel, true, false, null);
 	}
+	else if((position == false || position == 0) &&  this.realOn != false) {
+		this.realOn =  false;
+//                console.log("Dunkle Nacht.");
+		this.gateway.setValue(this.context.channel, false, false, null);
+	}
+  }
+  else {
 	this.gateway.setValue(this.context.channel, position, false, function(err, res) {
-          if (err) {
-            this.log.warn("Error setting " + this.context.name + " to " + (position ? "DEV2:on" : "DOFF2:off") + ": " + err);
-            //callback(err);
-          }
-          else {
-            this.log.info("Succeeded setting " + this.context.name + " to " + (position ? "DEV3:on" : "DOFF3:off") + ": ");
-            //callback(null);
-            if (position && this.context.duration) {
-                var service = this.getService(Service.Lightbulb) || this.getService(Service.Switch);
-                if (service) {
-                    setTimeout(function() {
-                    service.getCharacteristic(Characteristic.On).setValue(false);}, this.context.duration * 1000);
-                }
-            }
-          }
-    	}.bind(this));
-   }
+      		if (err) {
+        		  this.log.warn("Error setting " + this.context.name + " to " + (position ? "on" : "off") + ": " + err);
+        		  //callback(err);
+      		}
+      		else {
+          		this.log.info("Succeeded setting " + this.context.name + " to " + (position ? "on" : "off") + ": " + JSON.stringify(res));
+          		//callback(null);
+          		if (position && this.context.duration) {
+              			var service = this.getService(Service.Lightbulb) || this.getService(Service.Switch);
+              			if (service) {
+                  			setTimeout(function() {
+                  			service.getCharacteristic(Characteristic.On).setValue(false);}, this.context.duration * 1000);
+              			}
+          		}
+      		}
+  	}.bind(this));
+  }
 }
 
 
-
 function getBrightness(callback) {
-//  this.brightness = this.receivedBrightness;
+  console.log(this.accessories);
   this.log.info("getBrightness " + this.context.name + ": " + this.brightness);
   callback(null, this.brightness);
 }
 
 function setBrightness(brightness, callback) {
-  this.log.info("DEVMODESET:setBrightness " + this.context.name + ": " + brightness);
+  this.log.info("setBrightness " + this.context.name + ": " + brightness);
   callback(null);
 
   this.brightness = brightness;
-  this.receivedState = (brightness > 0) ? true : false;
+  if(brightness > 0) this.realOn = true;
   this.gateway.setValueDim(this.context.channel, brightness, function(err, res) {
       if (err) {
-          this.log.warn("setValueDim:Error setting " + this.context.name + " to " + this.brightness + ": " + err);
+          this.log.warn("Error setting " + this.context.name + " to " + this.brightness + ": " + err);
           //callback(err);
       }
       else {
-          this.log.info("setValueDim:Succeeded setting " + this.context.name + " to " + this.brightness + " : " + JSON.stringify(res));
-	  service.getCharacteristic(Characteristic.On).setValue(this.receivedState);
-//    this.service.getCharacteristic(Characteristic.Brightness
+          this.log.info("Succeeded setting " + this.context.name + " to " + this.brightness + " : " + JSON.stringify(res));
+
           //callback(null);
       }
   }.bind(this));
